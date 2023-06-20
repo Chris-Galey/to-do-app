f126093681e0312a6733906e4efcd264fbb16df3
GaleyDesign <christophergaleydesign@gmail.com>
2023-06-20 13:55:52 -0400

    implemented JWT

--- src/app/context/AuthContext.js
+++ src/app/context/AuthContext.js
@@ -1,32 +1,49 @@
 "use client";
 import { createContext, useState } from "react";
-
+import { useRouter } from "next/navigation";
 export const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [token, setToken] = useState(null);
-
+  const router = useRouter();
   console.log(user, token);
   const login = async (loginData) => {
-    console.log(loginData);
-    const response = await fetch("http://localhost:3000/login", {
-      method: "POST",
-      body: JSON.stringify(loginData),
-    });
-    const data = await response.json();
-    console.log(data);
-    // if (response.ok) {
-    //   setUser(data.user);
-    //   setToken(data.token);
-    // } else {
-    //   setUser(null);
-    //   setToken(null);
-    // }
+    try {
+      console.log(loginData);
+      const response = await fetch("http://localhost:3000/login", {
+        method: "POST",
+        body: JSON.stringify(loginData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+      console.log(data);
+
+      if (response.status === 200) {
+        setUser(loginData.email);
+        setToken(data.token);
+      } else {
+        setUser(null);
+        setToken(null);
+      }
+
+      // Store the token in session storage
+      sessionStorage.setItem("jwtToken", data.token);
+      router.push("http://localhost:3001/");
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   const logout = async () => {
     setUser(null);
+    setToken(null);
+
+    // Clear the token from session storage
+    sessionStorage.removeItem("jwtToken");
+
     console.log("logged out");
   };
 
--- src/app/page.js
+++ src/app/page.js
@@ -4,26 +4,52 @@ import { useState, useEffect } from "react";
 import styles from "./page.module.css";
 import Task from "./components/task/Task";
 import AddTask from "./components/task/AddTask";
-
+import { setToken } from "./context/AuthContext";
+import { useRouter } from "next/navigation";
 export default function Home() {
   const [data, setData] = useState([]);
   console.log(data);
+  const router = useRouter();
 
   useEffect(() => {
-    getTasks();
+    const storedToken = sessionStorage.getItem("jwtToken");
+
+    if (storedToken) {
+      getTasks(storedToken);
+    } else {
+      router.push("http://localhost:3001/login");
+    }
   }, []);
 
-  async function getTasks() {
-    const response = await fetch("http://localhost:3000/");
-    const data = await response.json();
-    setData(data);
+  async function getTasks(token) {
+    try {
+      const response = await fetch("http://localhost:3000/", {
+        headers: {
+          Authorization: token,
+        },
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setData(data);
+      } else {
+        // Handle unauthorized or other error responses
+        if (response.status === 401) {
+          // Redirect to login if unauthorized
+          router.push("/login");
+        } else {
+          // Handle other error responses
+          console.log("Error: ", response.status);
+        }
+      }
+    } catch (err) {
+      // Handle network or other errors
+      console.log("Error: ", err);
+    }
   }
-  const handleTaskChange = () => {
-    getTasks();
-  };
   return (
     <main className={styles.main}>
-      <AddTask onTaskChange={handleTaskChange} />
+      <AddTask onTaskChange={getTasks} />
       <section className={styles.container}>
         {data.map((task) => {
           return (
